73b708358d806f52e9e874599006537693d7d37f
Noushad Akhter <163678090+noushadakhter@users.noreply.github.com>
2024-04-23 23:16:33 +0500

    Add files via upload

---
+++ number guessing assignment/index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log(randomNumber);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-10: ",
+    },
+]);
+
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulation you Guess Right Number")
+}
+else{
+    console.log("you guess Wrong Number")
+}
